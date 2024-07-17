c83bc7a598028be117b61ca7e59c1c9d9567ae3e
Renan Locatiz Fernandes <renan.fernandes@outlook.com>
2024-07-17 12:35:20 -0300

    adicionado confirmação de devolução

--- js/app.js
+++ js/app.js
@@ -4,9 +4,14 @@ function alterarStatus(id) {
     let botao = jogoEscolhido.querySelector('.dashboard__item__button');
 
     if (imagem.classList.contains('dashboard__item__img--rented')){
+
+        let confirmarDevolucao = prompt("Tem certeza que quer devolver? Y para sim o N para cancelar devolução");
+
+        if (confirmarDevolucao == "Y" || confirmarDevolucao == "y"){
         imagem.classList.remove('dashboard__item__img--rented');
         botao.classList.remove('dashboard__item__button--return');
         botao.textContent = "Alugar";
+        }
     } else {
         imagem.classList.add('dashboard__item__img--rented');
         botao.classList.add('dashboard__item__button--return')
